e14e31b514921e6f39d5588d83c3fbbe8c21e55a
Niki <k.nikolett17@gmail.com>
2021-07-25 21:29:07 +0200

    Sign Up page with tests

--- src/pages/SignUp.spec.js
+++ src/pages/SignUp.spec.js
@@ -1,13 +1,133 @@
 import SignUp from './SignUp.svelte';
 import { render, screen } from '@testing-library/svelte';
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from "@testing-library/user-event";
+import 'whatwg-fetch';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 
+describe('Sign Up Page', () => {
+    describe('Layout', () => {
+        it('has sign up header', () => {
+            render(SignUp);
 
-it('has sign up header', () => {
-    render(SignUp);
+            const header = screen.getByRole('heading', {
+                name: 'Sign Up'
+            });
+            expect(header).toBeInTheDocument();
+        });
 
-    const header = screen.getByRole('heading', {
-        name: 'Sign Up'
+        it('has username input', () => {
+            const {container} = render(SignUp);
+
+            const input = container.querySelector('input[name="username"]');
+            expect(input).toBeInTheDocument();
+        });
+
+        it('has email input', () => {
+            const {container} = render(SignUp);
+
+            const input = container.querySelector('input[name="email"]');
+            expect(input).toBeInTheDocument();
+        });
+
+        it('has email type for email input', () => {
+            const {container} = render(SignUp);
+
+            const type = container.querySelector('input[name="email"]').type;
+            expect(type).toBe('email');
+        });
+
+        it('has password input', () => {
+            const {container} = render(SignUp);
+
+            const input = container.querySelector('input[name="password"]');
+            expect(input).toBeInTheDocument();
+        });
+
+        it('has password type for password input', () => {
+            const {container} = render(SignUp);
+
+            const type = container.querySelector('input[name="password"]').type;
+            expect(type).toBe('password');
+        });
+
+        it('has password repeat input', () => {
+            const {container} = render(SignUp);
+
+            const input = container.querySelector('input[name="password_repeat"]');
+            expect(input).toBeInTheDocument();
+        });
+
+        it('has password type for password repeat input', () => {
+            const {container} = render(SignUp);
+
+            const type = container.querySelector('input[name="password_repeat"]').type;
+            expect(type).toBe('password');
+        });
+
+        it('has a sign up button', () => {
+            const {container} = render(SignUp);
+
+            const button = container.querySelector('button');
+            expect(button).toBeInTheDocument();
+        });
+
+        it('disables the button initially', () => {
+            const {container} = render(SignUp);
+
+            const button = container.querySelector('button');
+            expect(button).toBeDisabled();
+        })
     });
-    expect(header).toBeInTheDocument();
+
+    describe('Interactions', () => {
+        it('enables the button if the two password field has matching values', async () => {
+            const {container} = render(SignUp);
+
+            const password = container.querySelector('input[name="password"]');
+            const passwordRepeat = container.querySelector('input[name="password_repeat"]');
+
+            await userEvent.type(password, 'p4ssword');
+            await userEvent.type(passwordRepeat, 'p4ssword');
+
+            const button = container.querySelector('button');
+            expect(button).toBeEnabled();
+        });
+
+        it('send data to backend on button submit', async () => {
+            let requestBody;
+            const server = setupServer(
+                rest.post(
+                    '/api/1.0/users',
+                    (request, response, context) => {
+                        requestBody = request.body;
+                        return response(context.status(200));
+                })
+            );
+            server.listen();
+
+            const {container} = render(SignUp);
+
+            const username = container.querySelector('input[name="username"]');
+            const email = container.querySelector('input[name="email"]');
+            const password = container.querySelector('input[name="password"]');
+            const passwordRepeat = container.querySelector('input[name="password_repeat"]');
+
+            await userEvent.type(username, 'demo');
+            await userEvent.type(email, 'demo@localhost');
+            await userEvent.type(password, 'demo');
+            await userEvent.type(passwordRepeat, 'demo');
+
+            const button = container.querySelector('button');
+            await userEvent.click(button);
+
+            await server.close();
+            expect(requestBody).toEqual({
+                username: 'demo',
+                email: 'demo@localhost',
+                password: 'demo',
+            });
+        });
+    })
 });
--- src/pages/SignUp.svelte
+++ src/pages/SignUp.svelte
@@ -1,1 +1,96 @@
+<script>
+    import Spinner from "../components/Spinner.svelte";
+
+    let isSubmiting = false;
+    let disabled;
+    let username = '';
+    let email = '';
+    let password = '';
+    let passwordRepeat = '';
+
+    $: disabled = password === '' || password !== passwordRepeat;
+
+    const submit = () => {
+        isSubmiting = true;
+        fetch('/api/1.0/users', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username, email, password
+            })
+        }).finally(() => isSubmiting = true);
+    }
+
+</script>
+
 <h1>Sign Up</h1>
+
+<form>
+    <label>
+        <span>Username</span>
+        <input bind:value={username} name="username" type="text" />
+    </label>
+    <label>
+        <span>Email</span>
+        <input bind:value={email} name="email" type="email" />
+    </label>
+    <label>
+        <span>Password</span>
+        <input bind:value={password} name="password" type="password" />
+    </label>
+    <label>
+        <span>Password repeat</span>
+        <input bind:value={passwordRepeat} name="password_repeat" type="password" />
+    </label>
+
+    <button type="submit" on:click|preventDefault={submit} {disabled}>
+        {#if isSubmiting}
+            <Spinner />
+        {/if}
+        Sign up
+    </button>
+</form>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+    form {
+        padding: 2rem;
+        background: #f3f3f3;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+
+    label {
+        display: inline-block;
+    }
+
+    span {
+        display: inline-block;
+        min-width: 120px;
+        max-width: 200px;
+    }
+
+    button {
+        cursor: pointer;
+        background: darkcyan;
+        border: none;
+        color: white;
+        padding: .5rem 1rem;
+    }
+
+    button:disabled {
+        opacity: .65;
+    }
+
+    button:not(:disabled) {
+        opacity: 1;
+        background: darkcyan;
+    }
+</style>
